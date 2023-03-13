3a44897641efadf306d105a2a74116787e107842
AgamjotSingh3010 <126556275+AgamjotSingh3010@users.noreply.github.com>
2023-03-13 14:17:44 +0530

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -3,5 +3,28 @@
 int main()
 {
   //add your code here
+	int limit;
+	scanf("enetr limit of N numbers:%d",&limit);
+  int Numbers[limit];
+  for(int i=0;i<limit;i++)
+	{
+		scanf("%d",&Numbers[i];
+  }
+	for(int i=0;i<limit;i++)
+	{
+		printf("%d  ",Numbers[i]);
+	}
+	int poscount=0,negcount=0;
+	for(int i=0;i<limit;i++)
+	{
+		for(Numbers[i]>0)
+		{
+			poscount++;
+		}
+		for(Numbers[i]<0)
+		{
+			negcount++;
+		}
+	}
   return 0;
 }
