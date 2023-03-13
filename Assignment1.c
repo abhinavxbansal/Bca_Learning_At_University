9541048d9ae7e899b4808f7ef46b3e4f1736d9d3
AgamjotSingh3010 <126556275+AgamjotSingh3010@users.noreply.github.com>
2023-03-13 14:31:36 +0530

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -1,30 +1,68 @@
 //Write a C program that allows the user to enter 'N' numbers and find the total positive numbers and total negative numbers using a for loop.
 
+// C program to find the count of positive
+// and negative integers in an array
+ 
+#include <stdio.h>
+ 
+// Function to find the count of
+// positive integers in an array
+int countPositiveNumbers(int* arr, int n)
+{
+ int pos_count = 0;
+ int i;
+ for (i = 0; i < n; i++) {
+  if (arr[i] > 0)
+   pos_count++;
+ }
+ return pos_count;
+}
+ 
+// Function to find the count of
+// negative integers in an array
+int countNegativeNumbers(int* arr, int n)
+{
+ int neg_count = 0;
+ int i;
+ for (i = 0; i < n; i++) {
+  if (arr[i] < 0)
+   neg_count++;
+ }
+ return neg_count;
+}
+ 
+// Function to print the array
+void printArray(int* arr, int n)
+{
+ int i;
+ 
+ printf("Array: ");
+ for (i = 0; i < n; i++) {
+  printf("%d ", arr[i]);
+ }
+ printf("\n");
+}
+ 
+// Driver program
 int main()
 {
-  //add your code here
-	int limit;
-	scanf("enetr limit of N numbers:%d",&limit);
-  int Numbers[limit];
+ int limit;
+ printf("eneter limit:");
+	scanf("%d",&limit);
+  int arr[limit];
   for(int i=0;i<limit;i++)
 	{
-		scanf("%d",&Numbers[i];
+		scanf("%d",&arr[i]);
   }
-	for(int i=0;i<limit;i++)
-	{
-		printf("%d  ",Numbers[i]);
-	}
-	int poscount=0,negcount=0;
-	for(int i=0;i<limit;i++)
-	{
-		for(Numbers[i]>0)
-		{
-			poscount++;
-		}
-		for(Numbers[i]<0)
-		{
-			negcount++;
-		}
-	}
-  return 0;
+ int n;
+ n = sizeof(arr) / sizeof(arr[0]);
+ 
+ printArray(arr, n);
+ 
+ printf("Count of Positive elements = %d\n",
+  countPositiveNumbers(arr, n));
+ printf("Count of Negative elements = %d\n",
+  countNegativeNumbers(arr, n));
+ 
+ return 0;
 }
