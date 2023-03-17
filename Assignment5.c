eec8986b2677e64b071e435d90db0febb0ae149b
AgamjotSingh3010 <126556275+AgamjotSingh3010@users.noreply.github.com>
2023-03-17 22:11:17 +0530

    Update Assignment5.c

--- Assignment5.c
+++ Assignment5.c
@@ -2,7 +2,34 @@
 //Given the score of 'N' students in the class, write a program to compute the maximum score. Use array to store the marks of the students and assume that the maximum number of students
 // in a class is 50. Assume that -1 is used to indicate a student who is absent for the exam. Assume that a student should secure a minimum of 50 marks to pass the exam.
 
-int main()
+#include <stdio.h>
+int main() 
 {
-  return 0;
+    int marks[50], n, max_score = -1, passed = 0, failed = 0;
+    printf("ENTER THE NUMBER OF STUDENTS (maximum 50): ");
+    scanf("%d", &n);
+    
+    printf("\nENTER THE MARKS OF EACH STUDENT HERE:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &marks[i]);
+        if (marks[i] > max_score) 
+		{
+		max_score = marks[i];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (marks[i] == -1) {
+            continue; // to skip absent students in exam
+        }
+        if (marks[i] >= 50) {
+            passed++;
+        } else {
+            failed++;
+        }
+    }
+    // results
+    printf("\n\t Maximum score is: %d\n", max_score);
+    printf("\n\t Number of students who are passed: %d\n", passed);
+    printf("\n\t Number of students who are failed: %d\n", failed);
+    return 0;
 }
