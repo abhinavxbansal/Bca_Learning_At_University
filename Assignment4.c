af8f7ec45f31623ce72fe24b8378feccfdb03218
abhinavxbansal <126556072+abhinavxbansal@users.noreply.github.com>
2023-03-18 20:54:13 +0530

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -1,7 +1,23 @@
 //Write a C code to find the maximum element in each row of the 3 X 3 matrix
 
+#include<stdio.h>
 int main()
 {
-  
-  return 0;
+int matrix[3][3]={{80,50,30},{76,45,65},{70,80,90}};
+int max=0,i,j;
+printf("MATRIX is :\n");
+for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+  {
+      printf("\t%d ",matrix[i][j]);
+	       if(max<matrix[i][j])
+	       {
+		       max=matrix[i][j];
+	       }
+  }
+  printf("\n");
+}
+  printf("maximum element in the matrix is %d",max);
+  return 0;    // name-Abhinav Bansal , roll no. -2210997011
 }
